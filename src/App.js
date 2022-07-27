2b7dcce1daa4f9c8b7b5cb3e31d515b2b7f8189f
klucas33 <klucas96@naver.com>
2022-07-27 23:36:38 +0900

    finally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="klucas33.github.io/movie_app/">
+      <Routes>
+        <Route element={Navigation} />
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={About} />
+        <Route path="/movie/:id" element={Detail} />
+      </Routes>
+    </Router>
   );
 }
 
